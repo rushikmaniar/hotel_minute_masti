86ab5f4935adafee93a39e2c0fcfa0dc53facd53
rushik maniar <rushikmaniar107@gmail.com>
2018-07-05 17:23:16 +0530

    codeigniter setup in progress.intial commit. gentela theme setup.

---
+++ application/views/backendwork/template/footerScript.php
@@ -0,0 +1,71 @@
+<?php // js scripts and custom.js ?>
+
+<!-- jQuery -->
+<script src="../vendors/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="../vendors/nprogress/nprogress.js"></script>
+<!-- Chart.js -->
+<script src="../vendors/Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -->
+<script src="../vendors/gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->
+<script src="../vendors/iCheck/icheck.min.js"></script>
+<!-- Skycons -->
+<script src="../vendors/skycons/skycons.js"></script>
+<!-- Flot -->
+<script src="../vendors/Flot/jquery.flot.js"></script>
+<script src="../vendors/Flot/jquery.flot.pie.js"></script>
+<script src="../vendors/Flot/jquery.flot.time.js"></script>
+<script src="../vendors/Flot/jquery.flot.stack.js"></script>
+<script src="../vendors/Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugins -->
+<script src="../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="../vendors/flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->
+<script src="../vendors/DateJS/build/date.js"></script>
+<!-- JQVMap -->
+<script src="../vendors/jqvmap/dist/jquery.vmap.js"></script>
+<script src="../vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="../vendors/moment/min/moment.min.js"></script>
+<script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="../build/js/custom.min.js"></script>
+
+
+<script type="text/javascript">
+
+    function ajaxModel(url, title, width) {
+        if (typeof(width) === 'undefined') {
+            width = 'modal-lg';
+        }
+        if (url) {
+            $.ajax({
+                url: SITE_URL + url,
+                dataType: 'html',
+                success: function (responce) {
+                    $('#feedback_admin_modal .modal-title').html(title);
+                    $('#feedback_admin_modal .modal-body').html(responce);
+                    $('#feedback_admin_modal .modal-dialog').addClass(width);
+
+                    if (!$('#feedback_admin_modal').hasClass('show')) {
+                        $('#feedback_admin_modal').modal('show');
+                    }
+
+                }
+            });
+        }
+    }
+    jQuery(document).ready(function (){
+
+    });
+</script>
